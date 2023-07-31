08fc4289faaa3d24486ba44164b4cec9084695e6
Jorge Munera <gerjo9211@hotmail.com>
2023-07-31 04:13:59 -0500

    filtro por nombre

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,10 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const MarketContext = createContext()
 
 const MarketContextProvider = ({children}) => {
     // estado para cantidad de productos total
     const [items, setItems] = useState([]);
+
+    // productos filtrados
+    const [filteredItems, setFilteredItems] = useState([]);
 
     // estado para ver el token y saber si esta logueado
     const [token, setToken] = useState(null);
@@ -14,10 +17,23 @@ const MarketContextProvider = ({children}) => {
     const showFilterAdmin = () => setIsAdministrator(true);
     const hideFilterAdmin = () => setIsAdministrator(false);
 
+    // estado para el logueo
     const [ isLogued, setIsLogued] = useState(false);
+
     // estado para productos a renderizar en order
     const [productsComprados, setProductsComprados] = useState([]);
 
+    // estado para el buscador
+    const [searchByName, setSearchByName] = useState('');
+
+
+    const filteredItemsByName = (items, searchByName) => {
+      return items?.filter((item) => item.name.toLowerCase().includes(searchByName.toLowerCase()))
+    }
+
+    useEffect(() => {
+      if(searchByName) setFilteredItems(filteredItemsByName(items, searchByName))
+    }, [items, searchByName])
 
     return (
         <MarketContext.Provider value={{
@@ -31,7 +47,10 @@ const MarketContextProvider = ({children}) => {
             productsComprados,
             setProductsComprados,
             isLogued,
-            setIsLogued
+            setIsLogued,
+            searchByName,
+            setSearchByName,
+            filteredItems
         }}>
             {children}
         </MarketContext.Provider>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,27 +5,53 @@ import { MarketContext } from "../../Context"
 import { FilterAdminPanel } from "../../Components/FilterAdminPanel"
 
 const Home = () => {
-    const context = useContext(MarketContext);
+  const context = useContext(MarketContext);
+  const renderView = () => {
+    if(context.searchByName?.length > 0){
+      if(context.filteredItems?.length > 0){
+        return (context.filteredItems?.map((item) => (
+          <Card
+            data={item}
+            key={item.id}
+          />
+        )))
+      } else {
+        return (
+          <div>No hay coincidencias :(</div>
+        )
+      }
+    } else {
+      return (context.items?.map((item) => (
+        <Card
+          data={item}
+          key={item.id}
+        />
+      )))
+    }
+  }
 
-    useEffect(() => {
-        fetch('http://localhost:3000/api/v1/products')
-            .then(response => response.json())
-            .then(data => context.setItems(data))
-    }, [])
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/products')
+      .then(response => response.json())
+      .then(data => context.setItems(data))
+  }, [])
 
-    return (
-        <Layout>
-            <div className="grid gap-4 gap-y-12 gap-x-4 grid-cols-4 max-w-screen-lg">
-                    {context.items?.map((item) => (
-                        <Card
-                            data={item}
-                            key={item.id}
-                        />
-                    ))}
-            </div>
-            {context.isAdministrator && <FilterAdminPanel />}
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div className='flex items-center justify-center relative w-80 mb-4'>
+        <h1 className='font-medium text-xl'>Productos</h1>
+      </div>
+      <input
+        type="text"
+        placeholder='Search a product'
+        className='rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none'
+        onChange={(event) => context.setSearchByName(event.target.value)} />
+      <div className="grid gap-4 gap-y-12 gap-x-4 grid-cols-4 max-w-screen-lg">
+        {renderView()}
+      </div>
+      {context.isAdministrator && <FilterAdminPanel />}
+    </Layout>
+  )
 }
 
 export { Home }
